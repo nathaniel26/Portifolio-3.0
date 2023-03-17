4ffc665cdb5050e1a34fed4611003d4313509aab
Nathaniel <nathaniel.farias92@gmail.com>
2023-03-17 17:09:27 -0300

    projetos completo

--- src/Componentes/ConteudosCorpo/Projetos.js
+++ src/Componentes/ConteudosCorpo/Projetos.js
@@ -48,51 +48,56 @@ function Projetos() {
         {step === 1 && (
           <div className={styles.caixa}>
             <img src={img1} alt="aa"></img>
-            <p>
-              aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa
-              aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa
-              aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa
-              aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa
-              aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa
-              aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa
-              aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa
-              aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa
-              aaaaa aaaaa
-            </p>
+            <div className={styles.desc}>
+              <h3>Geografia em jogos UFPel</h3>
+              <p>
+                O site do grupo de ensino, extensão e pesquisa do curso de
+                Licenciatura em Geografia da Universidade Federal de Pelotas
+                (UFPel) é a plataforma ideal para quem busca materiais de
+                qualidade sobre ensino de geografia com jogos. Nesse espaço,
+                disponibilizamos os materiais produzidos por nossa equipe, bem
+                como referências teóricas de autores que seguem essa mesma linha
+                de pesquisa.
+              </p>
+              <h3>React js - Html - Css</h3>
+              <h3>Site</h3>
+            </div>
           </div>
         )}
 
         {step === 2 && (
           <div className={styles.caixa}>
             <img src={img2} alt="aa"></img>
-            <p>
-              aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa
-              aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa
-              aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa
-              aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa
-              aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa
-              aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa
-              aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa
-              aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa
-              aaaaa aaaaa
-            </p>
+            <div className={styles.desc}>
+              <h3>Fly Fly Koronga</h3>
+              <p>
+                Fly Fly Koronga é um jogo divertido no estilo Flappy Bird. Nele,
+                o jogador precisa desviar da "vacina", o seu maior inimigo, e
+                ganha pontos à medida que avança. Além disso, é possível
+                escolher diferentes personagens para jogar. Experimente agora e
+                desafie seus amigos!
+              </p>
+              <h3>C#/Unity</h3>
+              <h3>Game - Android</h3>
+            </div>
           </div>
         )}
 
         {step === 3 && (
           <div className={styles.caixa}>
             <img src={img3} alt="aa"></img>
-            <p>
-              aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa
-              aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa
-              aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa
-              aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa
-              aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa
-              aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa
-              aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa
-              aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa aaaaa
-              aaaaa aaaaa
-            </p>
+            <div className={styles.desc}>
+              <h3>Homem vs Maquina</h3>
+              <p>
+                Você está pronto para enfrentar a máquina em 10 rodadas? O
+                objetivo do jogo é marcar o máximo de pontos possível para se
+                tornar o grande vencedor. Você tem três opções de jogada: pedra,
+                papel ou tesoura. Mas cuidado! A máquina também é esperta e está
+                pronta para te desafiar em cada rodada.
+              </p>
+              <h3>C#/Unity</h3>
+              <h3>Game - Android</h3>
+            </div>
           </div>
         )}
         <div className={styles.flechaDireita} onClick={handleRightArrow}>
@@ -125,7 +130,6 @@ function Projetos() {
           </>
         )}
       </div>
-      <h1>.</h1>
     </div>
   );
 }
